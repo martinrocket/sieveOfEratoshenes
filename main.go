57c4794db49cc9b8a00a6d3043300356e2958747
martinrocket <martin.ruble@gmail.com>
2025-06-26 11:14:44 -0600

    sorted out slice deletions.

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import ("fmt")
+/* The Sieve of Eratosthenes is an ancient algorithm for finding
+all prime numbers up to a specified integer. It works by
+iteratively marking the multiples of each prime number as composite,
+starting from 2, until reaching the square root of the specified limit. */
 
-func main(){
-  fmt.Println("Sieve of Eratoshenes")
-  }
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+	fmt.Println("\n")
+	fmt.Println("Sieve of Eratoshenes\n")
+
+	//Create a slice with 100 integers
+
+	var x []int
+
+	//for i := 2; i <= 100; i++ {
+	for i := 2; i <= 10; i++ {
+		x = append(x, i)
+
+	}
+
+	fmt.Println(x)
+	sliceLength := len(x)
+	for i := 0; i < sliceLength-1; {
+		x = slices.Delete(x, len(x)-1, len(x))
+		sliceLength = len(x)
+		fmt.Println(x)
+
+	}
+}
